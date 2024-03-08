d16d9c27ac94640c46e6672fd541b17eee6a5cf2
omrajpal <omrajpal15@gmail.com>
2024-03-07 21:33:23 -0500

    final changes

--- exer6/my-pokemon-app/pages/api/battle.js
+++ exer6/my-pokemon-app/pages/api/battle.js
@@ -1,7 +1,13 @@
 export default async function handler(req, res) {
     try {
+
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method Not Allowed' });
+    }
       // Extracting data from the request body
       const { pokemon1, pokemon2 } = req.body;
+      console.log(pokemon1)
+      console.log(pokemon2)
   
       // Fetch data for the first Pokemon
       const response1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon1}`);
@@ -10,13 +16,14 @@ export default async function handler(req, res) {
       // Fetch data for the second Pokemon
       const response2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon2}`);
       const data2 = await response2.json();
+      console.log(data2.stats)
   
       // Check if the data is valid
       if (data1 && data1.stats && data2 && data2.stats) {
         // Find the Pokemon with the higher base stat (assuming total base stats for simplicity)
         const totalBaseStat1 = data1.stats[0].base_stat;
         const totalBaseStat2 = data2.stats[0].base_stat;
-        
+
         const winningPokemon = totalBaseStat1 > totalBaseStat2 ? data1 : data2;
   
         res.status(200).json({

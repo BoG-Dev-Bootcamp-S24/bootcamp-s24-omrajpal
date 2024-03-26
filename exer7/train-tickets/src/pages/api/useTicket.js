c8a353a2b8b129f197f2510bfd85cb5dbc186c7b
omrajpal <omrajpal15@gmail.com>
2024-03-26 19:25:14 -0400

    final changes

--- exer7/train-tickets/src/pages/api/useTicket.js
+++ exer7/train-tickets/src/pages/api/useTicket.js
@@ -4,7 +4,7 @@ import deleteTicket from "../../../server/mongodb/actions/deleteTicket";
 export default async function handler(req, res) {
     if (req.method === 'DELETE') {
         try {
-            await deleteTicket(req.query.ticketID);
+            await deleteTicket(req.query.ticketId);
             return res.status(200).send('Success');
         } catch {
             return res.status(500).send('Failed');
